bdd6c1d4231b5383f0a864370565e44234352e4f
balak26 <bamlakgezahegn26@gmail.com>
2024-08-02 23:49:06 +0300

    added the playGame function that allows you to play for five rounds

--- main.js
+++ main.js
@@ -10,38 +10,49 @@ const getComputerChoice = () => {
 };
 
 const getHumanChoice = () => {
-  let choice = prompt("Enter Rock, Paper or Scissors to play");
+  let choice = prompt("Enter Rock, Paper or Scissors to play").toLowerCase();
 
   return choice;
 };
 
-let humanScore = 0;
-let computerScore = 0;
+const playGame = () => {
+  let humanScore = 0;
+  let computerScore = 0;
 
-const playRound = (humanChoice, computerChoice) => {
-  if (humanChoice === "rock" && computerChoice === "rock") {
-    return "Both are Rock, Its a tie";
-  } else if (humanChoice === "rock" && computerChoice === "paper") {
-    computerScore++;
-    return "Paper beats Rock, Computer wins";
-  } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    humanScore++;
-    return "Rock crushes Scissors, Player wins";
-  } else if (humanChoice === "paper" && computerChoice === "paper") {
-    return "Both are paper, Its a tie";
-  } else if (humanChoice === "paper" && computerChoice === "rock") {
-    humanScore++;
-    return "Paper beats Rock, Player wins";
-  } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    computerScore++;
-    return "Scissors cut papers, Computer wins ";
-  } else if (humanChoice === "scissors" && computerChoice === "scissors") {
-    return "Both are scissors, Its a tie";
-  } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    computerScore++;
-    return "Rock crushes Scissors, Computer wins";
-  } else if (humanChoice === "scissors" && computerChoice === "papers") {
-    humanScore++;
-    return "Scissors cut papers, Player wins ";
+  const playRound = (humanChoice, computerChoice) => {
+    if (humanChoice === "rock" && computerChoice === "rock") {
+      return "Both are Rock, Its a tie";
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+      computerScore++;
+      return "Paper beats Rock, Computer wins";
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+      humanScore++;
+      return "Rock crushes Scissors, Player wins";
+    } else if (humanChoice === "paper" && computerChoice === "paper") {
+      return "Both are paper, Its a tie";
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      humanScore++;
+      return "Paper beats Rock, Player wins";
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+      computerScore++;
+      return "Scissors cut papers, Computer wins ";
+    } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+      return "Both are scissors, Its a tie";
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+      computerScore++;
+      return "Rock crushes Scissors, Computer wins";
+    } else if (humanChoice === "scissors" && computerChoice === "papers") {
+      humanScore++;
+      return "Scissors cut papers, Player wins ";
+    }
+  };
+
+  for (let i = 1; i <= 5; i++) {
+    console.log(playRound(getComputerChoice(), getHumanChoice()));
+    console.log(
+      `Player Score: ${humanScore}, Computer Score: ${computerScore}`
+    );
   }
 };
+
+playGame();
