1877428fec06b91cc416386e93582f9aa0ec8d45
balak26 <bamlakgezahegn26@gmail.com>
2024-08-02 23:27:17 +0300

    Added playRound variable and score counting variables

--- main.js
+++ main.js
@@ -14,3 +14,34 @@ const getHumanChoice = () => {
 
   return choice;
 };
+
+let humanScore = 0;
+let computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+  if (humanChoice === "rock" && computerChoice === "rock") {
+    return "Both are Rock, Its a tie";
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore++;
+    return "Paper beats Rock, Computer wins";
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
+    return "Rock crushes Scissors, Player wins";
+  } else if (humanChoice === "paper" && computerChoice === "paper") {
+    return "Both are paper, Its a tie";
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
+    return "Paper beats Rock, Player wins";
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore++;
+    return "Scissors cut papers, Computer wins ";
+  } else if (humanChoice === "scissors" && computerChoice === "scissors") {
+    return "Both are scissors, Its a tie";
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore++;
+    return "Rock crushes Scissors, Computer wins";
+  } else if (humanChoice === "scissors" && computerChoice === "papers") {
+    humanScore++;
+    return "Scissors cut papers, Player wins ";
+  }
+};
