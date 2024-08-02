023f3d47af97814dc5d79308d7ea799d480351ef
balak26 <bamlakgezahegn26@gmail.com>
2024-08-02 23:00:38 +0300

    Created the functions getComputerChoice and getHumanChoice

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello from the console");
+const getComputerChoice = () => {
+  let choice = Math.floor(Math.random() * 3);
+  if (choice === 0) {
+    return "rock";
+  } else if (choice === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
+
+const getHumanChoice = () => {
+  let choice = prompt("Enter Rock, Paper or Scissors to play");
+
+  return choice;
+};
